3b7c208595c8feaa8bdaa13b38b4cffdb642be2f
masterUNG <phrombutr@gmail.com>
2020-07-17 14:25:40 +0700

    update

--- lib/widget/detail_product.dart
+++ lib/widget/detail_product.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rawinpornshop/models/search_model.dart';
+import 'package:rawinpornshop/utility/my_style.dart';
 
 class DetailProduct extends StatefulWidget {
   final SearchModel searchModel;
@@ -22,7 +23,30 @@ class _DetailProductState extends State<DetailProduct> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Text('name = ${model.name}'),
+      body: Column(
+        children: <Widget>[
+          buildText(context, 'รหัสสินค้า : ${model.code}'),
+          buildText(context, 'ชื่อสินค้า : ${model.name}'),
+        ],
+      ),
+    );
+  }
+
+  Widget buildText(BuildContext context, String string) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Card(
+          child: Container(
+            padding: EdgeInsets.all(8),
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Text(
+              string,
+              style: MyStyel().titleH2(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
