f617b3b28d159912677f3d4eebdd4f517c55afa6
masterUNG <phrombutr@gmail.com>
2020-09-25 10:42:02 +0700

    update

--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -1,9 +1,9 @@
 class MyConstant {
   String domain = 'http://210.86.171.110:89';
   String whidLatkabang = '7';
-  String whidEuamp = '';
-  String whidNongjok = '';
-  String whidHunjakai = '';
+  String whidEuamp = '8';
+  String whidNongjok = '9';
+  String whidHunjakai = '10';
 
   MyConstant();
 }
